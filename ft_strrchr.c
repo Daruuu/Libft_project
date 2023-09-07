0181a2ff77a1dbe953835f2cd9378f4f7f77057b
Daruny Salazar Cardoza <dasalaza@car2s1.42barcelona.com>
2023-09-07 20:03:34 +0200

    strchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,40 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/07 19:25:51 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/09/07 19:34:36 by dasalaza         ###   ########.fr       */
+/*   Created: 2023/09/07 19:47:13 by dasalaza          #+#    #+#             */
+/*   Updated: 2023/09/07 20:02:04 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
-/*localiza un caracter dentro de un puntero char.
-devuelve un puntero a la 1ra ocurrencia del caracter c en el string s */
-
-char    *ft_strchr(const char *s, int c)
+char	*strrchr(const char *s, int c)
 {
-    unsigned char   char_c;
-    int             i;
+	unsigned char	ch_c;
+	unsigned int	i;
 
-    i = 0;
-    char_c = (unsigned  char)c;
-    while (s[i] != '\0')
-    {
-        if (s[i] == char_c)
-            return ((char *) &s[i]);
-        i++;
-    }
-    return (0);
+	ch_c = (char )c;
+	i = sizeof(*s);
+	while (s[i] > 0)
+	{
+		if (s[i] == ch_c)
+		{
+			return ((char *)&s[i]);
+		}
+		i--;
+	}
+	return (0);
 }
-/*int main()
+
+int main()
 {
     const char *s;
     char *ptr;
     const char ch = 'o';
 
     s = "hello world";
-    ptr = ft_strchr(s, ch);
+    ptr = ft_strrchr(s, ch);
     printf("first occurrence of the '%c' in '%s' string is: '%s'", ch, s, ptr);
-}*/
+}
