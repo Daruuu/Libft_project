9aae1452da81de975aab52efa77d073ec2394603
Daruny Salazar Cardoza <dasalaza@cbr12s3.42barcelona.com>
2023-09-08 21:40:18 +0200

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,12 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 19:44:09 by dasalaza          #+#    #+#             */
+/*   Updated: 2023/09/08 21:39:55 by dasalaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  strlcpy(char *dst, const char *src, size_t size)
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
     size_t  i;
+    size_t  src_size;
 
+    if (!(dst || src))
+        return (0);
+    src_size = 0;
+
+    while (src [src_size])
+        src_size++;
     i = 0;
-    while ()
-
-
+    while (src[i] != '\0' && i < (dstsize - 1))
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (src_size);
 }
+/*int main()
+{
+    char    *buffer[19];
+    char    *src;
+
+    src = "hello world my friend";
+    len = 9;
+    size_t  result;
+
+    //result = ft_strlcpy((char *)buffer, src, len);
+    result = strlcpy((char *)buffer, src, len);
+    printf("%d",(int) result);
+
+    return (0);
+}*/
--- libft.h
+++ libft.h
@@ -6,13 +6,13 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 00:15:18 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/09/07 01:59:17 by dasalaza         ###   ########.fr       */
+/*   Updated: 2023/09/08 20:43:04 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-# include <stddef.h>
+# include <stddef.h> 
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -28,6 +28,6 @@ int	    ft_tolower(int c);
 char    *ft_strchr(const char *s, int c);
 char	*strrchr(const char *s, int c);
 int	    ft_strncmp(const char *s1, const char *s2, size_t n);
-
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
