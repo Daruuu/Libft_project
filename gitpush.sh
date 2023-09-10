9afc3f1e8af5e76dccd83e5c5a5a75fd2ecfe20f
Daruny Salazar Cardoza <dasalaza@car2s5.42barcelona.com>
2023-09-10 06:17:27 +0200

    upd gp

--- gitpush.sh
+++ gitpush.sh
@@ -1,9 +1,35 @@
 #script to automate my git push in every git repository
-git status --porcelain
+#!/bin/bash
+
+# Verificar el estado del repositorio
+if ! git status --porcelain; then
+    echo "No hay cambios para confirmar."
+    exit 1
+fi
+
+# Añadir todos los cambios
 git add .
-	read -p "Enter the commit message:" COMMITMESSAGE
+
+# Solicitar el mensaje del commit
+read -p "Enter the commit message: " COMMITMESSAGE
+
+# Confirmar los cambios
 git commit -m "$COMMITMESSAGE"
-	read -p "Enter the name of the branch (main):" BRANCH
+
+# Solicitar el nombre de la rama (por defecto, "main")
+read -p "Enter the name of the branch [main]: " BRANCH
+BRANCH=${BRANCH:-main}
+
+# Hacer un push a la rama especificada
 git push origin "$BRANCH"
-	echo "<<<<<<<<<<<<<<<REPOSITORY UPDATED SUCCESSFUL!!>>>>>>>>>>>>>>>"
+
+# Verificar si el push fue exitoso
+if [ $? -eq 0 ]; then
+    echo "REPOSITORY UPDATED SUCCESSFUL!"
+else
+    echo "Error al actualizar el repositorio."
+    exit 1
+fi
+
+# Mostrar los últimos 2 commits
 git log -n 2
