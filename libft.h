de77cd98748315a1a012c23259eeaaefb8734fb0
Daruny Salazar Cardoza <dasalaza@cbr5s1.42barcelona.com>
2023-09-12 15:18:41 +0200

    split

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 00:15:18 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/09/11 21:42:49 by dasalaza         ###   ########.fr       */
+/*   Updated: 2023/09/12 15:18:01 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,4 +50,5 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 #endif
