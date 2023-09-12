3f148450bb237dcc16fd1cb48a8084b22070962c
Daruuu <thedaruny16@gmail.com>
2023-09-12 13:11:36 +0200

    split structure

--- ft_split.c
+++ ft_split.c
@@ -11,10 +11,41 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
+
+static int ft_strlen_new(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+        i++;
+    return (i);
+}
+
+static size_t   ft_countCharacters(char const *s, char c)
+{
+
+    return ();
+}
 
 char    **ft_split(char const *s, char c)
 {
+    size_t  size_s;
+
+    size_s = ft_strlen_new((char *)s);
+    if (!s || !c)
+        return (0);
+
+    return (0);
+}
+
+int main()
+{
+    char const  *ss;
+    char find;
+
+    ss = "hello!zzzzzzzzz";
+    find = 'z';
 
     return (0);
 }
