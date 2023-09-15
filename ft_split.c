8198ac5d3cce5cc7b20ef82267b1bbe539a64eb5
Daruuu <thedaruny16@gmail.com>
2023-09-15 23:30:15 +0200

    split okay

--- ft_split.c
+++ ft_split.c
@@ -5,205 +5,56 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/10 09:54:20 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/09/15 00:56:04 by dasalaza         ###   ########.fr       */
+/*   Created: 2023/09/15 21:27:19 by dasalaza          #+#    #+#             */
+/*   Updated: 2023/09/15 23:12:17 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-/* dado un puntero busco 'c' y si la encuentro guardo la posicion total antes
- * de encontrar el caracter 'c'
- */
-
-static int ft_count_w(char const *s, char c)
+static int	ft_count_words(char const *s, char c)
 {
-    size_t count;
+	int	count;
 
-    if (!*s)
-        return (0);
-    count = 0;
-    while (*s)
-    {
-        while (*s == c)
-            s++;
-        if (*s)
-            count++;
-        while (*s != c && *s)
-            s++;
-    }
-    return (count);
-}
-/* 1. creamos un puntero doble que es donde vamos almacenar
- * el array de arrys como returno de funcion
- * 2. crear un malloc y comprobar que no sea NULL
- * 3. iterar por nuestro array que nos dan de parametro
- * 4. creamos un while donde iteramos por nuestro puntero "s"
- * 5. creamos un nuevo while, el cual comprobar que nuestro char "c"
- * existe en nuestro puntero "*s"
- * 6. mientras se cumplan estas 2 condiciones, avanzara el puntero "s"
- * 7. creamos un if en cual mientras puntero exista entara en el.
- * 8. utilizaremos la funcion "ft_strchr" (la cual busca un caracter
- * dentro de un puntero y retornar su posicion en el puntero.)
- * 9. cada vez que NO encuentre el caracter en el puntero, creara
- * un "strlen()" del puntero "s"(dado que no ha encontrado coincidencia.)
- * 10. creamos un else en cual actualizaremos en word_len usando la
- * la funcion ft_strchr() y le restaremos en puntero "s"
- * 11. rellenar el doble puntero en la posicion i++ y igualarlo
- * al "ft_substr""
- */
-
-char **ft_split(char const *s, char c)
-{
-    char    **table;
-    size_t  size_word;
-    int     i;
-
-    if (!(table == (char **)malloc(sizeof(char *) * (ft_count_w(s, c) + 1)))));
-        return (NULL);
-    i = 0;
-    while (*s)
-    {
-        while (*s == c && *s)
-            s++;
-        if (*s)
-        // return un puntero a la 1r character encontrado .
-        {
-            if (!ft_strchr(s, c))
-                size_word = ft_strlen(s);
-            else
-                size_word = ft_strchr(s, c) - s;
-            table[i++] = ft_substr(s, 0, size_word);
-            s = s + size_word;
-        }
-    }
-    table[i] = '\0';
-    return (table);
-}
-int main()
-{
-    char *str;
-
-    str = "hel lo wor /\0ld";
-    str = "aaaccssxxarqwer";
-    char c;
-
-    c = ' ';
-    c = 'a';
-
-    // str = "fffna, *%/n";
-    int countwords = ft_count_words(str, 33);
-    printf("%d", countwords);
-    return (0);
+	if (!s)
+		return (0);
+	count = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			count++;
+		while (*s && *s != c)
+			s++;
+	}
+	return (count);
 }
 
-/*
-static size_t ft_count_words(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    int counter;
+	char	**table;
+	int		i;
+	size_t	size_word;
 
-    if (!s || !c)
-        return (0);
-    counter = 0;
-    while (*s)
-    {
-        while (*s == c)
-            s++;
-        if (*s)
-            counter++;
-        while (*s && *s != c)
-            s++;
-    }
-    return (counter);
+	table = (char **)malloc((sizeof(char *) * (ft_count_words(s, c) + 1)));
+	if (!table || !s)
+		return (0);
+	i = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s)
+		{
+			if (!ft_strchr(s, c))
+				size_word = ft_strlen(s);
+			else
+				size_word = ft_strchr(s, c) - s;
+			table[i++] = ft_substr(s, 0, size_word);
+			s += size_word;
+		}
+	}
+	table[i] = '\0';
+	return (table);
 }
-
-static size_t   ft_count_chars(char const *s, char c)
-{
-    size_t  counter;
-
-    if (!s || !c)
-        return (0);
-    counter = 0;
-    while (*s)
-    {
-        while (*s == c)
-            s++;
-        if (*s)
-            counter++;
-        while (*s && *s != c)
-            s++;
-    }
-    return (counter);
-}
-static char *ft_strndup_split(char *str, size_t size)
-{
-    size_t i;
-    char *dup;
-
-    if (!str)
-        return (0);
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    dup = malloc(sizeof(char) * (i + 1));
-    if (!dup)
-        return (NULL);
-    i = 0;
-    while (str[i] != '\0')
-    {
-        dup[i] = str[i];
-        i++;
-    }
-    dup[i] = '\0';
-    return (dup);
-}
-
-char **ft_split(char const *s, char c)
-{
-    size_t i;
-    const char *start;
-    char **table;
-
-    if (!s || !c)
-        return (0);
-    if (!(table == (char **)malloc(sizeof(char *) * (ft_count_words(s, c) + 1))))
-        ;
-    return (NULL);
-    i = 0;
-    while (*s)
-    {
-        if (s[i] != c)
-        {
-            start = s;
-            while (*s && *s != c)
-                s++;
-            table[i++] = ft_strndup_split(start, s - start);
-            if (!table[i - 1])
-            {
-                while (i > 0)
-                    free(table[i]);
-                i--;
-            }
-            free(table);
-            return (NULL);
-        }
-        else
-            s++;
-    }
-    table [i] = '\0';
-    return (table);
-}
-
-int main()
-{
-    char const *ss;
-    char find;
-
-    ss = "      c   ";
-    find = ' ';
-    printf("%d\n", (int)ft_count_chars(ss, find));
-
-    return (0);
-}
-*/
--- ft_strchr.c
+++ ft_strchr.c
@@ -6,27 +6,27 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 17:24:48 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/09/11 17:28:53 by dasalaza         ###   ########.fr       */
+/*   Updated: 2023/09/15 21:24:21 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* localiza un caracter dentro de un puntero char'
  * return un puntero a la 1ra ocurrencia del caracter c en el string s
 */
+#include "libft.h"
+
 char	*ft_strchr(const char *s, int c)
 {
-	char	find;
 	int		i;
 
 	i = 0;
-	find = c;
-	while (s[i] != '\0')
+	while (s[i])
 	{
-		if (s[i] == find)
+		if (s[i] == (char)c)
 			return ((char *)&s[i]);
 		i++;
 	}
-	if (s[i] == find)
+	if (s[i] == (char)c)
 		return ((char *)&s[i]);
-	return (0);
+	return (NULL);
 }
