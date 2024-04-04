e02912befdfd5b263714b30cf9e4ff319c7be03d
Daruny Salazar Cardoza <dasalaza@car3s5.42barcelona.com>
2024-04-04 18:44:31 +0200

    add bonus

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,13 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   abc.c                                              :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daruuu <daruuu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/29 21:26:47 by daruuu            #+#    #+#             */
-/*   Updated: 2024/03/29 21:26:50 by daruuu           ###   ########.fr       */
+/*   Created: 2024/04/04 17:50:29 by dasalaza          #+#    #+#             */
+/*   Updated: 2024/04/04 18:44:13 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp_node;
+
+	if (!lst || !del)
+		return ;
+	while (lst && *lst)
+	{
+		tmp_node = tmp_node->next;
+		ft_lstdelone(*lst, del);
+
+	}
+}
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: dasalaza <dasalaza@student.42barcelona.c>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:11:48 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/04/04 12:51:50 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/04/04 17:48:50 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,44 @@ void    ft_lstdelone(t_list *lst, void (*del)(void *))
     }
 }
 
+/*
+void    del(void *content)
+{
+    free(content);
+}
+
 int main()
 {
+    t_list  *lista;
+    t_list  *node01;
+    t_list  *node02;
+    t_list  *node03;
+    t_list  *node04;
+
+    lista = NULL; 
+
+    node01 = (t_list *) malloc(sizeof(t_list));
+    node01->content = "node01";
+    node01->next = NULL;
+    lista = node01;
+
+    node02 = (t_list *) malloc(sizeof(t_list));
+    node02->content = "node02";
+    node02->next = NULL;
+    node01->next = node01;
+
+    node03 = (t_list *) malloc(sizeof(t_list));
+    node03->content = "node03";
+    node03->next = NULL;
+    node02->next = node03;
+
+    node04 = (t_list *) malloc(sizeof(t_list));
+    node03->content = "node03";
+    node04->next = NULL;
+    node03->next = node04;
+
+    ft_lstdelone(node02, del);
 
     return (0);
 }
+*/
