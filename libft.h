8dddec3975bc2383dd72af0b7388296053e1bdb1
Daruny Salazar Cardoza <dasalaza@car14s6.42barcelona.com>
2024-04-06 10:50:06 +0200

    all funct bonus, no testing

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 00:15:18 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/04/04 18:23:43 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/04/06 10:40:26 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ void	ft_putnbr_fd(int n, int fd);
 /*BONUS*/
 
 t_list  *ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
+void    ft_lstadd_front(t_list **lst, t_list *new);
 int     ft_lstsize(t_list *lst);
 t_list  *ft_lstlast(t_list *lst);
 void    ft_lstadd_back(t_list **lst, t_list *new);
@@ -77,5 +77,4 @@ void    ft_lstdelone(t_list *lst, void (*del)(void *));
 void    ft_lstclear(t_list **lst, void (*del)(void *));
 void    ft_lstiter(t_list *lst, void (*f)(void *));
 t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-
 #endif
