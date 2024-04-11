d2bda8b5a72d486d9dd8f3ddffdead5f9bbc633f
Daruny Salazar Cardoza <dasalaza@car14s5.42barcelona.com>
2024-04-11 17:44:52 +0200

    test add_back & lstdelone bonus

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 14:13:52 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/04/09 18:53:27 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/04/11 13:40:40 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,72 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*tmp;
+	t_list	*last_node;
 
-	tmp = ft_lstlast(*lst);
+	if (!lst || !new)
+		return ;
 	if (*lst == NULL)
 		*lst = new;
 	else
-		tmp->next = new;
+	{
+		last_node = *lst;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+		last_node->next = new;
+	}
 }
-
 /*
 int	main()
 {
+	t_list	*lista;
+	t_list	*node1;
+	t_list	*node2;
+	t_list	*node3;
+    t_list	*node4;
+    t_list	*node5;
+
+	lista = NULL;
+
+	node1 = (t_list *) malloc(sizeof(t_list));
+	node1->content = "First";
+	node1->next = NULL;
+	lista = node1;
+
+	node2 = (t_list *) malloc(sizeof(t_list));
+	node2->content = "second";
+	node2->next = NULL;
+	node1->next = node2;
+
+	node3 = (t_list *) malloc(sizeof(t_list));
+	node3->content = "third";
+	node3->next = NULL;
+	node2->next = node3;
+
+    node4 = (t_list *) malloc(sizeof(t_list));
+    node4->content = "fourth";
+    node4->next = NULL;
+    node3->next = node4;
+
+	node5 = (t_list *) malloc(sizeof(t_list));
+	node5->content = "five";
+	node5->next = NULL;
+	node4->next = node5;
+	
+	// testing here
+	int	size = ft_lstsize(lista);
+	printf("length of lista is: %d\n", size);
+
+	t_list	*node = NULL;
+	node = (t_list *) malloc(sizeof(t_list));
+	node->content = "hello world";
+	node->next = NULL;
+
+	printf("lst_add_back\n");
+	ft_lstadd_back(&lista, node);
+
+	size = 0;
+	size = ft_lstsize(lista);
+	printf("length of lista after test is: %d\n", size);
 
 	return (0);
 }
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,26 +6,27 @@
 /*   By: dasalaza <dasalaza@student.42barcelona.c>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:11:48 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/04/09 19:00:41 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/04/11 14:12:52 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstdelone(t_list *lst, void (*del)(void *))
-{
-    if (lst)
-    {
-        del(lst->content);
-        free(lst);
-    }
-}
-/*
 void    del(void *content)
 {
     free(content);
 }
 
+void    ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+    if (!lst && !del)
+    {
+        del(lst->content);
+        free(lst);
+    }
+}
+
+/*
 int main()
 {
     t_list  *lista;
@@ -33,6 +34,7 @@ int main()
     t_list  *node02;
     t_list  *node03;
     t_list  *node04;
+    t_list  *node05;
 
     lista = NULL; 
 
@@ -56,8 +58,18 @@ int main()
     node04->next = NULL;
     node03->next = node04;
 
+    node05 = (t_list *) malloc(sizeof(t_list));
+    node05->content = "nuevo contenido";
+    node05->next = NULL;
+    node04->next = node05;
+
+    printf("size of lista: %d \n", ft_lstsize(lista));
+
+    printf("execute ft_lstdelone\n");
     ft_lstdelone(node02, del);
+
+    printf("size of lista: %d \n", ft_lstsize(lista));
 
     return (0);
 }
-*/
+    */
